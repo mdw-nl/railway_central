faf9c0e07e772bb3e2d5187191f5132bd8249326
tim hendriks <tim.hendriks@medicaldataworks.nl>
2019-12-11 09:47:45 +0100

    Outline backend project.

---
+++ src/test/java/nl/medicaldataworks/railway/central/CentralApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.medicaldataworks.railway.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

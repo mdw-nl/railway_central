2e08c917d99a44e85fc2365e0c274cc99f0ebc5f
Tim <tim.lustberg@medicaldataworks.nl>
2019-12-15 22:20:22 +0100

    fixed the post. the get is still broken (see todo)

--- src/main/java/nl/medicaldataworks/railway/central/web/controller/TrainTaskController.java
+++ src/main/java/nl/medicaldataworks/railway/central/web/controller/TrainTaskController.java
@@ -5,12 +5,15 @@ import nl.medicaldataworks.railway.central.domain.Task;
 import nl.medicaldataworks.railway.central.domain.Train;
 import nl.medicaldataworks.railway.central.repository.TaskRepository;
 import nl.medicaldataworks.railway.central.repository.TrainRepository;
+import nl.medicaldataworks.railway.central.util.KeycloakUtil;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,39 +24,41 @@ import java.util.Optional;
 public class TrainTaskController {
     private TaskRepository taskRepository;
     private TrainRepository trainRepository;
+    private KeycloakUtil keycloakUtil = new KeycloakUtil();;
 
     public TrainTaskController(TaskRepository taskRepository, TrainRepository trainRepository) {
         this.taskRepository = taskRepository;
         this.trainRepository = trainRepository;
     }
 
-    @GetMapping("/train/{id}/tasks")
+    @GetMapping("/trains/{id}/tasks")
     public ResponseEntity<List<Task>> getTasks(@PathVariable Long id, Authentication authentication) {
         log.debug("REST request to get tasks : {}", id);
-        List<Task> tasks = taskRepository.findByTrainIdAndOwnerName(id, authentication.getName());
+        //TODO Why is train null when taskRepository.findAll()?
+        List<Task> tasks = taskRepository.findByTrainIdAndOwnerName(id, keycloakUtil.getPreferredUsernameFromAuthentication(authentication));
         return ResponseEntity.ok(tasks);
     }
 
-//    @PostMapping("train/{id}/tasks")
-//    public ResponseEntity<Task> createTask(@PathVariable Long id, @RequestBody Task task,
-//                                           Authentication authentication) throws Exception {
-//        log.debug("REST request to save Task : {}", task);
-//        if (task.getId() != null) {
-//            throw new Exception("A new task cannot already have an ID");
-//        }
-//        Optional<Train> train = trainRepository.findById(id);
-//        Train validTrain = train.orElseThrow(() -> new Exception("No valid train for supplied ID."));
-//        if(authentication.getName() != null && authentication.getName().equals(validTrain.getOwnerName())) {
-//            validTrain.getTasks().add(task);
-//            Train result = trainRepository.save(validTrain);
-//            return ResponseEntity.created(new URI("/api/tasks/" + result.getId()))
-//                    .body(result);
-//        } else {
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-//        }
-//    }
+    @PostMapping("trains/{id}/tasks")
+    public ResponseEntity<Task> createTask(@PathVariable Long id, @RequestBody Task task,
+                                           Authentication authentication) throws Exception {
+        log.debug("REST request to save Task : {}", task);
+        if (task.getId() != null) {
+            throw new Exception("A new task cannot already have an ID");
+        }
+        Optional<Train> train = trainRepository.findById(id);
+        Train validTrain = train.orElseThrow(() -> new Exception("No valid train for supplied ID."));
+        if(keycloakUtil.getPreferredUsernameFromAuthentication(authentication) != null && keycloakUtil.getPreferredUsernameFromAuthentication(authentication).equals(validTrain.getOwnerName())) {
+            task.setOwnerName(validTrain.getOwnerName());
+            validTrain.getTasks().add(task);
+            Train result = trainRepository.save(validTrain);
+            return ResponseEntity.created(new URI("/api/tasks/" + result.getId())).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
 
-    @PutMapping("train/{id}/tasks")
+    @PutMapping("trains/{id}/tasks")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task, Authentication authentication)
             throws Exception {
         log.debug("REST request to update task : {}", task);
@@ -62,7 +67,7 @@ public class TrainTaskController {
         }
         Optional<Train> train = trainRepository.findById(id);
         Train validTrain = train.orElseThrow(() -> new Exception("No valid train for supplied ID."));
-        if(authentication.getName() != null && authentication.getName().equals(validTrain.getOwnerName())) {
+        if(keycloakUtil.getPreferredUsernameFromAuthentication(authentication) != null && keycloakUtil.getPreferredUsernameFromAuthentication(authentication).equals(validTrain.getOwnerName())) {
             Task result = taskRepository.save(task);
             return ResponseEntity.ok()
                     .body(result);
@@ -71,12 +76,12 @@ public class TrainTaskController {
         }
     }
 
-//    @DeleteMapping("train/{id}/tasks/{id}")
+//    @DeleteMapping("trains/{id}/tasks/{id}")
 //    public ResponseEntity<Void> deleteTask(@PathVariable Long id, Authentication authentication) {
 //        log.debug("REST request to delete task : {}", id);
 //        Optional<Task> task = taskRepository.findById(id);
-//        if(authentication.getName() != null && task.isPresent() &&
-//                authentication.getName().equals(task.get().getOwnerName())){
+//        if(keycloakUtil.getPreferredUsernameFromAuthentication(authentication) != null && task.isPresent() &&
+//                keycloakUtil.getPreferredUsernameFromAuthentication(authentication).equals(task.get().getOwnerName())){
 //            taskRepository.deleteById(id);
 //            return ResponseEntity.noContent().build();
 //        }else {

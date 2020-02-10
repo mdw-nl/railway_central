469971fbea68f2dfe37aed2702d99e36788d5aa8
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-10 15:13:18 +0100

    First version deploy step.

---
+++ containers/deploy.sh
@@ -0,0 +1,11 @@
+docker pull registry.gitlab.com/medicaldataworks/railway/central:$1
+
+#docker stop central
+#docker rm central
+
+docker run -d \
+    --name central \
+    --network=railway \
+    -v /opt/central/application.yml:/app/application.yml \
+    -p 8042:8042 \
+    registry.gitlab.com/medicaldataworks/railway/central:$1

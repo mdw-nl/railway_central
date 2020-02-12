05d5f5f0ea25d562060314ffbdf4659ca1fd7236
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-12 16:27:11 +0100

    Improving shell scripting to add entities.

--- add_tasks.sh
+++ add_tasks.sh
@@ -5,7 +5,11 @@ access_token=$(curl -X POST -u "$timl_name:$timl_secret" -d "grant_type=client_c
 
 response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"","stationId":1, "iteration":0, "master": false, "input":"2"}')
 echo "add client task 1 response: $response"
-response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"","stationId":1, "iteration":0, "currentIteration":0, "master": false, "input":"6"}')
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"","stationId":2, "iteration":0, "currentIteration":0, "master": false, "input":"6"}')
+echo "add client task 2 response: $response"
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"","stationId":3, "iteration":0, "currentIteration":0, "master": false, "input":"7"}')
+echo "add client task 2 response: $response"
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"","stationId":4, "iteration":0, "currentIteration":0, "master": false, "input":"8"}')
 echo "add client task 2 response: $response"
 response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"REQUESTED","result":"","stationId":1, "iteration":0, "master": true, "input":""}')
 echo "add master task response: $response"

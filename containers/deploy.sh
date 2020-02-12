684a42532005da88642629cf0f4e8e5d3f7f09b0
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-12 11:28:49 +0100

    Removed network railway from deploy.

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -1,10 +1,9 @@
 docker pull registry.gitlab.com/medicaldataworks/railway/central:$1
 
 docker stop central
-docker rm central
+#docker rm central
 
 docker run -d \
     --name central \
-    --network=railway \
     -p 8080:8080 \
     registry.gitlab.com/medicaldataworks/railway/central:$1

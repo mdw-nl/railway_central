32fcbbef5f9c201ffb7b0fdbb2be1ba73bbf0e0c
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-13 13:57:14 +0100

    Replaced all webclient instances with resttemplate again, getting the access token manually for now.

--- add_stations.sh
+++ add_stations.sh
@@ -8,11 +8,11 @@ ananya_name='ananya'
 ananya_secret='48996987-604f-4604-a462-01db2e16b925'
 
 access_token=$(curl -X POST -u "$timl_name:$timl_secret" -d "grant_type=client_credentials" https://dcra-keycloak.railway.medicaldataworks.nl/auth/realms/railway/protocol/openid-connect/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
-response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'timl_name'"}')
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$timl_name'"}')
 echo "add station response: $response"
-response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'timh_name'"}')
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$timh_name'"}')
 echo "add station response: $response"
-response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'johan_name'"}')
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$johan_name'"}')
 echo "add station response: $response"
-response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'ananya_name'"}')
+response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$ananya_name'"}')
 echo "add station response: $response"

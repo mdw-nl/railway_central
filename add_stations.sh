ece7e8bc83592f76c3e5fc5a8287392382db5bff
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-18 16:04:06 +0100

    Updated secrets.

--- add_stations.sh
+++ add_stations.sh
@@ -1,11 +1,11 @@
 timl_name='timl'
-timl_secret='65d9ab8f-94ad-4320-af80-d55549de8b62'
+timh_secret='b70c3982-cad2-43c8-8eef-14391aa373e6'
 timh_name='timh'
 johan_name='johan'
 ananya_name='ananya'
 maastro_name='maastro'
 
-access_token=$(curl -X POST -u "$timl_name:$timl_secret" -d "grant_type=client_credentials" https://dcra-keycloak.railway.medicaldataworks.nl/auth/realms/railway/protocol/openid-connect/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
+access_token=$(curl -X POST -u "$timh_name:$timh_secret" -d "grant_type=client_credentials" https://dcra-keycloak.railway.medicaldataworks.nl/auth/realms/railway/protocol/openid-connect/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
 response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$timl_name'"}')
 echo "add station response: $response"
 response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/stations?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"name":"'$timh_name'"}')
--- add_tasks.sh
+++ add_tasks.sh
@@ -1,7 +1,7 @@
 train_id=$1
-timl_name='timl'
-timl_secret='65d9ab8f-94ad-4320-af80-d55549de8b62'
-access_token=$(curl -X POST -u "$timl_name:$timl_secret" -d "grant_type=client_credentials" https://dcra-keycloak.railway.medicaldataworks.nl/auth/realms/railway/protocol/openid-connect/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
+timh_name='timh'
+timh_secret='b70c3982-cad2-43c8-8eef-14391aa373e6'
+access_token=$(curl -X POST -u "$timh_name:$timh_secret" -d "grant_type=client_credentials" https://dcra-keycloak.railway.medicaldataworks.nl/auth/realms/railway/protocol/openid-connect/token | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
 
 response=$(curl --verbose --location --request POST https://dcra.railway.medicaldataworks.nl/api/trains/$train_id/tasks?access_token=$access_token --header 'Content-Type: application/json' --data-raw '{"creationTimestamp":"2019-07-02T04:14:00.742+0000","calculationStatus":"COMPLETED","result":"{\"calculation_result\": 6}","stationId":1, "iteration":0, "currentIteration":0, "master": false, "input":""}')
 echo "add client task 2 response: $response"

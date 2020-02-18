7f33a2a3ba6ef996e5bca5e901fc7f78f89b14ff
tim hendriks <tim.hendriks@medicaldataworks.nl>
2020-02-18 14:50:50 +0100

    Fixing build issues.

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -6,5 +6,6 @@ docker rm central
 docker run -d \
     --name central \
     -p 8080:8080 \
-    -v /opt/central/containers/appdata-mount:/app/ \
+    -v /opt/central/containers/application.yml:/app/application.yml \
+    -v /opt/central/containers/database:/app/database \
     registry.gitlab.com/medicaldataworks/railway/central:$1
